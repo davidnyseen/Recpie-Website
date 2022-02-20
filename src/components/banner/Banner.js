c00b4b7ad408ee660356282c395650566ed4ca93
david nyssen <davidnyssen22@gmail.com>
2022-02-20 12:09:02 +0200

    not sure if works

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -29,8 +29,8 @@ export default function App() {
         <nav className="main-nav">
           <ul>
 
-            <li><Link to="/">Home</Link></li>
             <li><Link to="categories">Categories</Link></li>
+            <li><Link to="/">Home</Link></li>
             <li><Link to="create">Create recipe</Link></li>
             <li><Link to="myaccount">My account</Link></li>
             <li><Link to="about">About</Link></li>
